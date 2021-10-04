5d4af0f1b0f904546b04a9b9f7c19f9e45794fd6
subho004 <91646273+subho004@users.noreply.github.com>
2021-10-04 09:00:19 +0530

    fibonacci.cpp
    created a c++ program to print fibonacci series in given interval

---
+++ fibonacci.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+//c++ program to print fibonacci series in given interval
+//Fibonacci number is a series of numbers in which each number is the sum of the two preceding numbers.
+//contributed by subho004
+
+int main() {
+    int n, t1 = 0, t2 = 1, nextTerm = 0;
+
+    cout << "Enter the number of terms: ";
+    cin >> n;
+
+    cout << "Fibonacci Series: ";
+
+    for (int i = 1; i <= n; ++i) {
+        // Prints the first two terms.
+        if(i == 1) {
+            cout << t1 << ", ";
+            continue;
+        }
+        if(i == 2) {
+            cout << t2 << ", ";
+            continue;
+        }
+        nextTerm = t1 + t2;
+        t1 = t2;
+        t2 = nextTerm;
+        
+        cout << nextTerm << ", ";
+    }
+    return 0;
+}
